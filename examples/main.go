cdcce2a369ff2576f319f1b062ec12807851a21a
fanchann <fannyarifnass@gmail.com>
2024-06-24 12:10:43 +0700

    examples

---
+++ examples/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "github.com/fanchann/guml"
+
+type Customers struct {
+	CustomerID string `guml:"customer_id"`
+	Name       string `guml:"name"`
+	Email      string `guml:"email"`
+}
+
+type Orders struct {
+	OrderID    string `guml:"order_id"`
+	CustomerID string `guml:"customer_id"`
+}
+
+type Products struct {
+	ProductID   string `guml:"product_id"`
+	ProductName string `guml:"product_name"`
+}
+
+type OrdersProducts struct {
+	OrderProductID string `guml:"order_product_id"`
+	OrderID        string `guml:"order_id"`
+	ProductID      string `guml:"product_id"`
+}
+
+func main() {
+	cust := Customers{}
+	order := Orders{}
+	prod := Products{}
+	orderProd := OrdersProducts{}
+
+	g := guml.New()
+
+	g.Entity(cust).
+		Entity(order).
+		Entity(prod).
+		Entity(orderProd).
+		OneToMany(cust, order).
+		OneToMany(order, orderProd).
+		ManyToOne(orderProd, prod).
+		Gen("examples")
+}
